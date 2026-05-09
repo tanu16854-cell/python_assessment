802ccfdcab44aed7d4b162b5c9f86afe536e07af
Tanu Rana <tanu16854@gmail.com>
2026-05-09 09:54:30 +0530

    First commit - Python assessment project

---
+++ Question_1.py
@@ -0,0 +1,32 @@
+# Q1: Count uppercase, lowercase, digits, spaces, special characters
+
+text = input("Enter a paragraph: ")
+
+upper = lower = digits = spaces = special = 0
+
+for ch in text:
+    if ch.isupper():
+        upper += 1
+    elif ch.islower():
+        lower += 1
+    elif ch.isdigit():
+        digits += 1
+    elif ch.isspace():
+        spaces += 1
+    else:
+        special += 1
+
+result = {
+    "Uppercase": upper,
+    "Lowercase": lower,
+    "Digits": digits,
+    "Spaces": spaces,
+    "Special Characters": special
+}
+
+# Sorting by frequency (descending)
+sorted_result = dict(sorted(result.items(), key=lambda x: x[1], reverse=True))
+
+print("\nCharacter Frequency (Descending Order):")
+for k, v in sorted_result.items():
+    print(k, ":", v)
